0413ebe08a38130ae9cee486ca7d165748e5f1a2
staFF6773 <nosenivel3@gmail.com>
2025-06-03 19:24:20 -0600

    Páleta de colores

--- src/ui/src/components/views/HomeView.vue
+++ src/ui/src/components/views/HomeView.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+import { colors } from '../../lib/themes/colors';
 </script>
 
 <template>
-    <div class="p-2">
-        <div>
-            home pending
+    <div class="p-2" :style="{ backgroundColor: colors.background, color: colors.text }">
+        <div class="p-4 rounded">
+            <h1 class="text-2xl mb-4">Home View Test</h1>
+            <button 
+                class="px-4 py-2 rounded" 
+                :style="{ backgroundColor: colors.button }"
+            >
+                Test Button
+            </button>
         </div>
     </div>
 </template>
